08e34ab330d49377bf76d6f10e97566a4e8e15d1
Shinrai <tronox@hotmail.com>
2018-03-05 00:39:17 -0800

    Added verbose mode + code cleanup.

--- test/test-mem.js
+++ test/test-mem.js
@@ -3,7 +3,11 @@ const sizeofvar = require('../sizeofvar.js');
 
 function test(obname, ob) {
 	console.log('=== '+obname+' ===');
+	if (argv.v) {
+		console.log(ob);
+	}
 	var l = sizeof2(ob);
+	// for some reason some variables will return lower values during process.memoryUsage if ran a 2nd time.
 	l = sizeof2(ob);
 	console.log(obname+' [sizeof2]: '+l);
 	l = sizeofvar(ob);
@@ -27,10 +31,6 @@ function sizeof2(o) {
 	start_memory = process.memoryUsage().heapUsed;
 	start_memory = process.memoryUsage().heapUsed;
 
-	start_memory = process.memoryUsage().heapUsed;
-	start_memory = process.memoryUsage().heapUsed;
-	start_memory = process.memoryUsage().heapUsed;
-
 	t = JSON.parse(tt);
 	
 	return (process.memoryUsage().heapUsed - start_memory);
@@ -42,6 +42,10 @@ const argv = yargs
     .command('test', 'Which test to run')
     .example('$0 object', 'Runs the test specified')
 	.demandCommand(1, 'You need at least one command before moving on')
+	.option('verbose', {
+		alias: 'v',
+		default: false
+	})
     .help('h')
     .alias('h', 'help')
     .epilog('Copyright 2018 - Catalyzed Motivation Inc')
@@ -53,6 +57,3 @@ if (typeof argv._ != "undefined" && argv._ != '') {
 		test(key,o);
 	});
 }
- // else {
-	// yargs.help();
-// }
